a1b6450de082b37e6f1da9528ea562a982dd9ac1
krupenmax <krupendeu@gmail.com>
2022-05-15 18:26:08 +0300

    Edit View Add made

--- musicPlayer/base/urls.py
+++ musicPlayer/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from base.models import Track
-from .views import TrackList
+from .views import TrackCreate, TrackDetail, TrackList, TrackCreate, TrackUpdate
 
 urlpatterns = [
-     path('', TrackList.as_view(), name='Tracks'),
+     path('', TrackList.as_view(), name='tracks'),
+     path('track/<int:pk>/', TrackDetail.as_view(), name='track'),
+     path('track-create/', TrackCreate.as_view(), name = "track-create"),
+     path('track-update/<int:pk>/', TrackUpdate.as_view(), name = "track-update")
 ]
--- musicPlayer/base/views.py
+++ musicPlayer/base/views.py
@@ -1,10 +1,27 @@
+from re import template
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Track
 # Create your views here.
 
 class TrackList(ListView):
     model = Track
-    context_object_name = "Tracks"
+    context_object_name = 'tracks'
 
-    
+class TrackDetail(DetailView):
+    model = Track
+    context_object_name = 'track'
+    template_name = 'base/track_detail.html'
+
+class TrackCreate(CreateView):
+    model = Track
+    fields = '__all__'
+    success_url = reverse_lazy('tracks')
+
+class TrackUpdate(UpdateView):
+    model = Track
+    fields = '__all__'
+    success_url = reverse_lazy('tracks')
