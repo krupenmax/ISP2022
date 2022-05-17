e1ebf3ee60affafa1989c4fd77bf35110283fb67
krupenmax <krupendeu@gmail.com>
2022-05-17 20:14:04 +0300

    Register, login, edit, delete added

--- musicPlayer/base/urls.py
+++ musicPlayer/base/urls.py
@@ -1,11 +1,17 @@
+from ast import Delete
 from django.urls import path
 
-from base.models import Track
-from .views import TrackCreate, TrackDetail, TrackList, TrackCreate, TrackUpdate
+from base.models import Track, Genre
+from .views import TrackCreate, TrackDetail, TrackList, TrackCreate, TrackUpdate, TrackDelete, CustomLoginView, RegisterView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+     path('login/', CustomLoginView.as_view(), name='login'),
+     path('register/', RegisterView.as_view(), name='register'),
+     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
      path('', TrackList.as_view(), name='tracks'),
      path('track/<int:pk>/', TrackDetail.as_view(), name='track'),
      path('track-create/', TrackCreate.as_view(), name = "track-create"),
-     path('track-update/<int:pk>/', TrackUpdate.as_view(), name = "track-update")
+     path('track-update/<int:pk>/', TrackUpdate.as_view(), name = "track-update"),
+     path('track-delete/<int:pk>/', TrackDelete.as_view(), name = "track-delete")
 ]
--- musicPlayer/base/views.py
+++ musicPlayer/base/views.py
@@ -1,27 +1,76 @@
+from distutils.log import Log
 from re import template
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Track
 # Create your views here.
 
-class TrackList(ListView):
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    filed = '__all__'   
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tracks')
+
+
+class RegisterView(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tracks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tracks')
+        return super(RegisterView, self).get(*args, **kwargs)
+
+class TrackList(LoginRequiredMixin, ListView):
     model = Track
     context_object_name = 'tracks'
 
-class TrackDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tracks'] = context['tracks'].filter(user=self.request.user)
+        return context
+
+
+class TrackDetail(LoginRequiredMixin, DetailView):
     model = Track
     context_object_name = 'track'
     template_name = 'base/track_detail.html'
 
-class TrackCreate(CreateView):
+
+class TrackCreate(LoginRequiredMixin, CreateView):
     model = Track
-    fields = '__all__'
+    fields = ['title', 'artist', 'genre', 'description']
     success_url = reverse_lazy('tracks')
 
-class TrackUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TrackCreate, self).form_valid(form)
+
+
+class TrackUpdate(LoginRequiredMixin, UpdateView):
     model = Track
-    fields = '__all__'
+    fields = ['title', 'artist', 'genre', 'description']
+    success_url = reverse_lazy('tracks')
+
+class TrackDelete(LoginRequiredMixin, DeleteView):
+    model = Track
+    context_object_name = 'track'
     success_url = reverse_lazy('tracks')
