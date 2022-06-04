77f758b01e7cf477f3eaa8cdc6dedcf4364037b7
Max <max.krupen2003@gmail.com>
2022-06-04 22:36:08 +0300

    Little changes added

--- musicPlayer/musicPlayer/settings.py
+++ musicPlayer/musicPlayer/settings.py
@@ -135,7 +135,7 @@ LOGIN_URL = 'login'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
