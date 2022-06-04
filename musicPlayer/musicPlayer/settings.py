cf35b87690cacf3cc32e502e4dc7b8468f64cd35
Max <max.krupen2003@gmail.com>
2022-06-04 22:22:00 +0300

    Little changes added

--- musicPlayer/musicPlayer/settings.py
+++ musicPlayer/musicPlayer/settings.py
@@ -129,6 +129,9 @@ LOGIN_URL = 'login'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
